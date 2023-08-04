570c39b822c1a74ec3f2017dc57de3583a688855
josemacielfj <josemacielfj@gmail.com>
2023-08-04 01:56:19 -0300

    Exigindo chave privada do cliente

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "025cf79ffa8f6c8572a4bce32e023768c635e3ccdea0fd82c4bbe1478d88dcacd3": 100,
+  "03104ab89f8082d9e79dca1a0f48675fd8ed9b5f01ad237433f3572e746a78145d": 50,
+  "038b982b65aaabf221bcd40bdecbe23c1f6d55b355dbd16b7def743af2a98a7580": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,9 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature from de client-side application
+  //recover the public address from the signature
+  
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
